994661e81e6853e2da550751f3a95b03a44580eb
Jakub Wicher <galaxykuby@gmail.com>
2024-04-06 10:19:34 +0200

    add filtering

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
 import draggable from 'vuedraggable'
-import { ref, watch, onBeforeMount } from 'vue'
+import { watch, onBeforeMount, shallowRef } from 'vue'
 
 /*
 Example usage:
   data format:
     const data = [
-      { h1: 1, h2: 2, h3: 3 },
-      { h1: 4, h2: 5, h3: 6 },
-      { h1: 7, h2: 8, h3: 9 }
+      { h1: 1, h2: 2, h3: true  },
+      { h1: 4, h2: 5, h3: false },
+      { h1: 7, h2: 8, h3: false }
     ]
 
     const headers = ['h1', 'h2', 'h3']
 
+    <DataTable :data="data" :headers="headers" />
+
+
+  custom columns display:
     const customColumns = {
       h1: TextColumn // TextColumn is vue component with data property
     }
@@ -20,24 +24,61 @@ Example usage:
     you can also override columns inside DataTable body with dynamic slot:
     <template #col(column header)={data}>,
     for example:
-    
+
     <DataTable ...>
       <template #col(h1)={data}>
          <h1>{{ data }}</h1>
       <template />
     <DataTable />
+
+
+  filtering data:
+    filtering works by passing object of column-name:filter-value.
+    you an also modify what the filter value has to be in order to show the value by defining object
+    with value (filter value) and modifier (function that takes in filter value and cell value and returns boolean)
+    that will define filter behavior, eg.
+
+    const filters = reactive({
+      global: '',
+      h1: '',
+      h2: '',
+      h3: {
+        value: '',
+        modifier: (filterV: string, cellV: boolean) => {
+          const v = filterV.toLowerCase()
+          if (cellV) {
+            return v === 'yes'
+          } else {
+            return v === 'no'
+          }
+        }
+      }
+    })
+
+    <DataTable ... :filters="filters" />
 */
 
-const tableUUID = crypto.randomUUID()
+type CellValue = number | string | boolean | object
+type RowEntry = { [key: string]: CellValue }
+type DataType = RowEntry[]
+type Header = string
 
 const props = withDefaults(
   defineProps<{
-    data: { [key: string]: number | string | boolean | object }[]
-    headers: string[]
-    customColumns: { [key: string]: any }
+    data: DataType
+    headers: Header[]
+    customColumns: { [key: Header]: any }
+    filters: {
+      [key: Header]:
+        | string
+        | { value: string; modifier: (filterVal: string, cellVal: CellValue) => boolean }
+    } & {
+      global?: string
+    }
   }>(),
   {
-    customColumns: () => ({})
+    customColumns: () => ({}),
+    filters: () => ({})
   }
 )
 
@@ -47,23 +88,69 @@ type HeadersModel = {
   header: string
 }
 
-const headersModel = ref<HeadersModel[]>([])
+const headersModel = shallowRef<HeadersModel[]>([])
 
-const updateHeadersModel = (value: string[]) => {
+const updateHeadersModel = (value: Header[]) => {
   headersModel.value = value.map((header) => ({
-    __id: `table:${tableUUID}&header:${header}`,
+    __id: `h:${header}`,
     header
   }))
 }
 watch(() => props.headers, updateHeadersModel, { deep: true })
 /* END HEADERS */
 
-/* COLUMNS */
-/* END COLUMNS */
+/* FILTERS */
+const filteredData = shallowRef<DataType>([])
+
+const updateFilteredData = (value: DataType) => {
+  const definedFilters = Object.keys(props.filters)
+  if (definedFilters.length === 0) {
+    filteredData.value = value
+    return
+  }
+
+  // write a function that filters data based on filter values and puts the result in filteredData
+  const isGlobalFilter = props.filters.global !== undefined
+
+  filteredData.value = props.data.filter((dataRow) => {
+    let isFilteredOut = false
+    const isGloballyFilteredOut =
+      isGlobalFilter &&
+      props.filters.global!.length &&
+      !Object.values(dataRow).some((val) => {
+        return String(val).toLowerCase().includes(props.filters.global!.toLowerCase())
+      })
+
+    for (const [dataKey, dataValue] of Object.entries(dataRow)) {
+      const keyInFilters = Object.prototype.hasOwnProperty.call(props.filters, dataKey)
+      if (!keyInFilters) {
+        continue
+      }
+
+      const filterValue = props.filters[dataKey]
+      if (typeof filterValue === 'object') {
+        if (filterValue.value.length && !filterValue.modifier(filterValue.value, dataValue)) {
+          isFilteredOut = true
+        }
+      } else {
+        if (!String(dataValue).toLowerCase().includes(filterValue.toLocaleLowerCase())) {
+          isFilteredOut = true
+        }
+      }
+    }
+
+    return !isFilteredOut && !isGloballyFilteredOut
+  })
+}
+
+watch(() => props.filters, updateFilteredData, { deep: true })
+watch(() => props.data, updateFilteredData, { deep: true })
+/* END FILTERS */
 
 /* INIT */
 onBeforeMount(() => {
   updateHeadersModel(props.headers)
+  updateFilteredData(props.data)
 })
 /* END INIT */
 </script>
@@ -81,8 +168,8 @@ onBeforeMount(() => {
     </thead>
 
     <tbody>
-      <tr v-for="(dataEntry, rowIdx) in props.data" :key="`${tableUUID}-r:${rowIdx}`">
-        <td v-for="header in headersModel" :key="`${tableUUID}-r:${rowIdx}h:${header.header}`">
+      <tr v-for="(dataEntry, rowIdx) in filteredData" :key="`r:${rowIdx}`">
+        <td v-for="header in headersModel" :key="`r:${rowIdx}h:${header.header}`">
           <slot
             v-if="$slots[`col(${header.header})`]"
             :data="dataEntry[header.header]"
@@ -104,3 +191,12 @@ onBeforeMount(() => {
     </tbody>
   </table>
 </template>
+
+<style scoped>
+table,
+th,
+td {
+  border: 1px solid white;
+  border-collapse: collapse;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,14 +1,31 @@
 <script setup lang="ts">
 import DataTable from '../components/DataTable.vue'
-import { ref, computed, watch } from 'vue'
+import { ref, computed, reactive, onMounted } from 'vue'
 import TextColumn from '../components/columns/TextColumn.vue'
 import AlertColumn from '../components/columns/AlertColumn.vue'
 
-const headers = ref([
+const headers = ref<{ vis: boolean; val: 'h1' | 'h2' | 'h3' }[]>([
   { vis: true, val: 'h1' },
   { vis: true, val: 'h2' },
   { vis: true, val: 'h3' }
 ])
+
+const filters = reactive({
+  global: '',
+  h1: '',
+  h2: '',
+  h3: {
+    value: '',
+    modifier: (filterV: string, cellV: CellValue) => {
+      const v = filterV.toLowerCase()
+      if (cellV) {
+        return v === 'tak'
+      } else {
+        return v === 'nie'
+      }
+    }
+  }
+})
 
 const visibleHeaders = computed(() => headers.value.filter((h) => h.vis).map((h) => h.val))
 
@@ -17,15 +34,48 @@ const customColumns: { [key: string]: any } = {
   h2: AlertColumn
 }
 
-const data = [
-  { h1: 1, h2: 2, h3: 3 },
-  { h1: 4, h2: 5, h3: 6 },
-  { h1: 7, h2: 8, h3: 9 }
-]
+const data = ref([
+  { h1: 'test', h2: 2, h3: true },
+  { h1: 'test', h2: 1, h3: true },
+  { h1: 'test', h2: 2, h3: false },
+  { h1: 'test1', h2: 2, h3: true },
+  { h1: 'test1', h2: 5, h3: false },
+  { h1: 'test2', h2: 8, h3: false }
+])
+
+onMounted(() => {
+  function generateRandomObject() {
+    const randomObject = {
+      h1: generateRandomString(),
+      h2: Math.floor(Math.random() * 100), // Generate a random integer between 0 and 99 for h2
+      h3: Math.random() < 0.5 // Generate a random boolean for h3
+    }
+    return randomObject
+  }
+
+  function generateRandomString() {
+    const length = Math.floor(Math.random() * 10) + 1 // Generate a random string length between 1 and 10
+    let result = ''
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length))
+    }
+    return result
+  }
+
+  for (let i = 0; i < 500; i++) {
+    data.value.push(generateRandomObject())
+  }
+})
 </script>
 
 <template>
   <div>
+    <div v-for="filterKey in Object.keys(filters) as (keyof typeof filters)[]" :key="filterKey">
+      filter {{ filterKey }}
+      <input v-if="typeof filters[filterKey] === 'string'" v-model="filters[filterKey]" />
+      <input v-else v-model="filters[filterKey].value" />
+    </div>
     <div v-for="h in headers" :key="h.val">
       <input
         type="checkbox"
@@ -41,7 +91,12 @@ const data = [
     </div>
   </div>
 
-  <DataTable v-bind:headers="visibleHeaders" :data="data" :customColumns="customColumns">
+  <DataTable
+    v-bind:headers="visibleHeaders"
+    :data="data"
+    :customColumns="customColumns"
+    :filters="filters"
+  >
     <template #headerItem="{ data }">
       <h2>
         {{ data }}
